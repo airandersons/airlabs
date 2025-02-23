f38d62042b790ab7a6bd5ca82fd761977b2a47a2
Mwanje Isma Andersons <airlabs-ug@AirLabs.airlabs.net>
2025-02-23 20:02:30 +0300

    fixed the comment section form display and comment display  and worked on team section

--- airlabs/main_site/forms.py
+++ airlabs/main_site/forms.py
@@ -6,13 +6,13 @@ class TestimonialsForm(forms.ModelForm):
         model = Testimonials
         fields = ['name', 'profession', 'comment', 'image']
 
-class PostCommentForm():
-    class meta:
+class PostCommentForm(forms.ModelForm):
+    class Meta:
         model = PostComments
         fields = ['author_name', 'author_email', 'author_image', 'comment']
         widgets = {
             'author_name': forms.TextInput(attrs={'type': 'text', 'class': 'form-control', 'placeholder': 'Your Name'}),
-            'author_email': forms.TextInput(attrs={'type': 'email', 'class': 'form-control', 'placeholder': 'Your Name'}),
-            'author_image': forms.FileInput(attrs={'type': 'file', 'class': 'form-control'}),
-            'comment': forms.Textarea(attrs={'type': 'textarea', 'class': 'form-control', 'placeholder': 'Write your comment here....', 'rows': '5'})
+            'author_email': forms.TextInput(attrs={'type': 'email', 'class': 'form-control', 'placeholder': 'Email'}),
+            'author_image': forms.ClearableFileInput(attrs={'type': 'file', 'class': 'form-control'}),
+            'comment': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Write your comment here....', 'rows': '5'})
         }
